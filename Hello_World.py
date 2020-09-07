b90319ffa0cb4e9d76c7336bc21f27ff42349dd5
prideven <70807326+prideven@users.noreply.github.com>
2020-09-07 16:45:41 +0530

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,16 +1,16 @@
 import os
-import socket
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 hostname = socket.gethostname()
-host = socket.gethostbyname(hostname)
+hostIP = socket.gethostbyname(hostname)
 
 @app.route("/")
 def index():
     ret= "Hello World from %s" %(hostname)
     return ret
 
-app.run(host=host, port=8080)
+app.run(host=hostIP, port=8080)
 
